c25bce0a62f8364bf1b7b198eda9b6fa1da2e0dd
RikaZa <jrr.zambrano@yavirac.edu.ec>
2021-06-26 10:24:13 -0500

    Inicio

---
+++ Deber_Arboles/Deber_Arboles/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Deber_Arboles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
