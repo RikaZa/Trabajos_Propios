84b8957f9ca7604f61c4c0d445ab0c334d9fc24b
JRZR1723 <zajo.deox@gmail.com>
2021-06-29 00:45:33 -0500

    Nuevo_ Act -- >>

--- Deber_Arboles.entidades/Contador.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Deber_Arboles.entidades
-{
-    public class Contador : Departamento
-    {
-        public string NombreContador { get; set; }
-        public string ApellidoContador { get; set; }
-    }
-}
